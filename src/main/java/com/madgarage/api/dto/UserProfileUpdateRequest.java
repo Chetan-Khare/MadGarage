5e60e70393222b98cb1b9680abdde78dafe15068
Chetan-Khare <ckhare55@gmail.com>
2026-04-16 19:34:37 +0530

    added some conditions

--- src/main/java/com/madgarage/api/dto/UserProfileUpdateRequest.java
+++ src/main/java/com/madgarage/api/dto/UserProfileUpdateRequest.java
@@ -6,15 +6,15 @@ import lombok.Data;
 
 @Data
 public class UserProfileUpdateRequest {
-    @Size(max = 50, message = "First name too long")
+    @Size(max = 10, message = "First name too long")
     private String firstName;
 
-    @Size(max = 50, message = "Last name too long")
+    @Size(max = 10, message = "Last name too long")
     private String lastName;
 
     @Email(message = "Invalid email format")
     private String email;
 
-    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    @Size(min = 6, max = 12, message = "Password must be between 6 and 12 characters")
     private String password;
 }
